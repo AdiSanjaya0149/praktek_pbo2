7c171d0ac806d89d9ca21e29361d055d30498280
adi0149.it@gmail.com <adi16630149>
2018-05-06 23:22:37 +0800

    Membuat Attribut dan Behavior

--- src/praktek_pbo2/square.java
+++ src/praktek_pbo2/square.java
@@ -1,4 +1,12 @@
 package praktek_pbo2;
 public class square {
-    
+    double sisi;
+   
+    void cetakInfo(){
+        System.out.println("=================");
+        System.out.println("sisi : "+sisi);
+        System.out.println("=================");
+        
+                
+    }
 }
