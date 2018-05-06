cbb771d014069cea103873f39f2ef68b6575d293
adi0149.it@gmail.com <adi16630149>
2018-05-06 23:49:08 +0800

    Contructur - sama dengan nama class

--- src/praktek_pbo2/square.java
+++ src/praktek_pbo2/square.java
@@ -1,7 +1,11 @@
 package praktek_pbo2;
 public class square {
     double sisi;
-   
+
+    public square() {
+        sisi=10;
+    }
+    
     void cetakInfo(){
         System.out.println("=================");
         System.out.println("sisi : "+sisi);
@@ -13,5 +17,8 @@ public class square {
         double luas;
         luas=sisi*sisi;
         return luas;
+    }
+      void cetakLuas(){
+        System.out.println("Luasnya adalah:"+hitungLuas());
     }
 }
